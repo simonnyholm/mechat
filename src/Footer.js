6c26c7236b30571aa1d23902163821aede7e996c
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-08-05 12:21:42 +0200

    simplify footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <footer>
-      <section id="settings"><h2>Indstillinger</h2></section>
-      <section>Copyright Bertrambygger INC.</section>
+      <section id="settings">
+        <h1>Indstillinger</h1>
+      </section>
     </footer>
   );
 };
